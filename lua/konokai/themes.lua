abcc217600f4e5067eab8c4bf1070e4c36d02f77
AdrienDML <adriendml99@protonmail.com>
2022-11-05 12:11:16 +0100

    Added LuaLine theme

---
+++ lua/konokai/themes.lua
@@ -0,0 +1,31 @@
+local themes = {}
+local function lualine_theme()
+    local colors = require'konokai'.palette
+    local bg = string.lower(colors.background.hex)
+    local fg = string.lower(colors.foreground.hex)
+    local green = string.lower(colors.green.hex)
+    local blue = string.lower(colors.blue.hex)
+    local orange = string.lower(colors.orange.hex)
+    local yellow = string.lower(colors.yellow.hex)
+    local puple = string.lower(colors.purple.hex)
+    local grey = string.lower(colors.grey.hex)
+    return {
+        normal = {
+            a = {fg = bg,  bg = green},
+            b = {fg = bg, bg = blue},
+            c = {fg = fg, bg = bg}
+        },
+        insert = {a = {fg = bg, bg = orange}},
+        visual = {a = {fg = bg, bg = yellow}},
+        replace = {a = {fg = bg, bg = red}},
+        inactive = {
+            a = {fg = purple, bg = bg},
+            b = {fg = fg, bg = purple},
+            c = {fg = grey, bg = bg}
+        }
+    }
+end
+themes.lualine = lualine_theme()
+
+
+return themes
