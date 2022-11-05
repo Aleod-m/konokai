e52fd4d09c7ca29a10b00761e2d85248dd498559
AdrienDML <adriendml99@protonmail.com>
2022-11-05 08:38:41 +0100

    Fixed forgotten commas

--- lua/lush_theme/konokai.lua
+++ lua/lush_theme/konokai.lua
@@ -305,17 +305,17 @@ local theme = lush(function()
   }
 end)
 theme.palette = {
-    background = hsl("#191919")
-    foreground = hsl("#c4c4b5")
-    green      = hsl("#97e023")
-    orange     = hsl("#fa8419")
-    yellow     = hsl("#dfd561")
-    purple     = hsl("#9c64fe")
-    red        = hsl("#f3005f")
-    cyan       = hsl("#57d1ea")
-    blue       = hsl("#57d1ea").da(30)
-    grey       = hsl("#c4c4b5").da(30)
-    white      = hsl("#f6f6ee")
+    background = hsl("#191919"),
+    foreground = hsl("#c4c4b5"),
+    green      = hsl("#97e023"),
+    orange     = hsl("#fa8419"),
+    yellow     = hsl("#dfd561"),
+    purple     = hsl("#9c64fe"),
+    red        = hsl("#f3005f"),
+    cyan       = hsl("#57d1ea"),
+    blue       = hsl("#57d1ea").da(30),
+    grey       = hsl("#c4c4b5").da(30),
+    white      = hsl("#f6f6ee"),
 }
 
 -- Return our parsed theme for extension or use elsewhere.
