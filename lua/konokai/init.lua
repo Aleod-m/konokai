a486942f50283d13df3956ca0192d444d6b507cd
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2025-05-13 18:20:38 +0200

    Fix forgotten comma.

--- lua/konokai/init.lua
+++ lua/konokai/init.lua
@@ -3,18 +3,18 @@ local lush = require'lush'
 local hsl = lush.hsl 
 
 K.palette = {
-  black      = hsl("#191919")
-  green      = hsl("#97e023")
-  orange     = hsl("#fa8419")
-  yellow     = hsl("#dfd561")
-  purple     = hsl("#9c64fe")
-  pink       = hsl("#fe71fa")
-  red        = hsl("#f3005f")
-  cyan       = hsl("#57d1ea")
-  blue       = hsl("#577cea")
-  grey       = hsl("#929276")
-  grey_light = hsl("#c4c4b5")
-  white      = hsl("#f6f6ee")
+  black      = hsl("#191919"),
+  green      = hsl("#97e023"),
+  orange     = hsl("#fa8419"),
+  yellow     = hsl("#dfd561"),
+  purple     = hsl("#9c64fe"),
+  pink       = hsl("#fe71fa"),
+  red        = hsl("#f3005f"),
+  cyan       = hsl("#57d1ea"),
+  blue       = hsl("#577cea"),
+  grey       = hsl("#929276"),
+  grey_light = hsl("#c4c4b5"),
+  white      = hsl("#f6f6ee"),
 }
 
 return K
